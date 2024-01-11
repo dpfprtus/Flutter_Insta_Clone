106d91ca5a86d9ac18bceb6d6d4417c2a6cfeeb8
suyong choi <dpfprtus@gmail.com>
2024-01-12 00:03:12 +0900

    [FEAT] 업로드 Layout

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -8,13 +8,89 @@ class Upload extends StatelessWidget {
   Widget _imagePreview() {
     return Container(
       width: Get.width,
-      height: Get.height,
+      height: 200,
       color: Colors.grey,
     );
   }
 
   Widget _header() {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Row(
+              children: [
+                Text(
+                  '갤러리',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_drop_down),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                decoration: BoxDecoration(
+                    color: const Color(0xff808080),
+                    borderRadius: BorderRadius.circular(30)),
+                child: Row(children: [
+                  ImageData(IconPath.imageSelectIcon),
+                  const SizedBox(
+                    width: 7,
+                  ),
+                  const Text(
+                    '여러 항목 선택',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  )
+                ]),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Container(
+                padding: const EdgeInsets.all(6),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xff808080),
+                ),
+                child: ImageData(IconPath.cameraIcon),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _imageSelectList() {
+    return GridView.builder(
+        //상위 위젯과 스크롤이 겹쳐 오류가 나기 때문에 GridView의 스크롤 기능 정지
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          mainAxisSpacing: 1,
+          crossAxisSpacing: 1,
+          childAspectRatio: 1,
+        ),
+        itemCount: 100,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.red,
+          );
+        });
   }
 
   @override
@@ -58,7 +134,7 @@ class Upload extends StatelessWidget {
         child: Column(children: [
           _imagePreview(),
           _header(),
-          // _imageSelectList(),
+          _imageSelectList(),
         ]),
       ),
     );
