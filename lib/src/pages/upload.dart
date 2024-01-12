5370bbb7c480ccfbd3a199c6dce271cbcf4ce7ea
suyong choi <dpfprtus@gmail.com>
2024-01-12 16:51:59 +0900

    [FEAT] 이미지 선택

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -1,15 +1,80 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_clone_instagram/src/components/image_data.dart';
-import 'package:get/get.dart';
+import 'package:photo_manager/photo_manager.dart';
 
-class Upload extends StatelessWidget {
+class Upload extends StatefulWidget {
   const Upload({super.key});
 
+  @override
+  State<Upload> createState() => _UploadState();
+}
+
+class _UploadState extends State<Upload> {
+  var albums = <AssetPathEntity>[];
+  var headerTitle = '';
+  var imageList = <AssetEntity>[];
+  AssetEntity? selectedImage;
+  void _loadPhotos() async {
+    var result = await PhotoManager.requestPermissionExtend();
+
+    if (result.isAuth) {
+      albums = await PhotoManager.getAssetPathList(
+        type: RequestType.image,
+        filterOption: FilterOptionGroup(
+            imageOption: const FilterOption(
+              sizeConstraint: SizeConstraint(minHeight: 100, minWidth: 100),
+            ),
+            orders: [
+              const OrderOption(
+                type: OrderOptionType.createDate,
+                asc: false,
+              ),
+            ]),
+      );
+      _loadData();
+    } else {
+      print('권한이없습니다');
+      await PhotoManager.openSetting();
+      //권한 요청
+    }
+  }
+
+  void _loadData() async {
+    headerTitle = albums.first.name;
+    await _pagingPhotos();
+    update();
+  }
+
+  Future<void> _pagingPhotos() async {
+    var photos = await albums.first.getAssetListPaged(page: 0, size: 30);
+    imageList.addAll(photos);
+    selectedImage = imageList.first;
+  }
+
+  void update() => setState(() {});
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadPhotos();
+  }
+
   Widget _imagePreview() {
+    var width = MediaQuery.of(context).size.width;
     return Container(
-      width: Get.width,
-      height: 200,
+      width: width,
+      height: width,
       color: Colors.grey,
+      child: selectedImage == null
+          ? Container()
+          : _photoWidget(selectedImage!, width.toInt(), builder: (data) {
+              return Image.memory(
+                data,
+                fit: BoxFit.cover,
+              );
+            }),
     );
   }
 
@@ -19,19 +84,78 @@ class Upload extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const Padding(
-            padding: EdgeInsets.all(5.0),
-            child: Row(
-              children: [
-                Text(
-                  '갤러리',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
+          GestureDetector(
+            onTap: () {
+              //터치하면 밑에서 올라온다.
+              showModalBottomSheet(
+                context: context,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
                   ),
                 ),
-                Icon(Icons.arrow_drop_down),
-              ],
+                isScrollControlled: albums.length > 10 ? true : false,
+                constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height -
+                      MediaQuery.of(context).padding.top,
+                  minHeight: 200,
+                ),
+                builder: (_) => SizedBox(
+                  height: albums.length > 10
+                      ? Size.infinite.height
+                      : albums.length * 60,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Center(
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 7),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.black54,
+                            ),
+                            width: 40,
+                            height: 4,
+                          ),
+                        ),
+                        Expanded(
+                          child: SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: List.generate(
+                                albums.length,
+                                (index) => Container(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                    horizontal: 20,
+                                  ),
+                                  child: Text(
+                                    albums[index].name,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ]),
+                ),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Row(
+                children: [
+                  Text(
+                    headerTitle,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const Icon(Icons.arrow_drop_down),
+                ],
+              ),
             ),
           ),
           Row(
@@ -76,20 +200,46 @@ class Upload extends StatelessWidget {
 
   Widget _imageSelectList() {
     return GridView.builder(
-        //상위 위젯과 스크롤이 겹쳐 오류가 나기 때문에 GridView의 스크롤 기능 정지
-        physics: const NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          mainAxisSpacing: 1,
-          crossAxisSpacing: 1,
-          childAspectRatio: 1,
-        ),
-        itemCount: 100,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            color: Colors.red,
+      //상위 위젯과 스크롤이 겹쳐 오류가 나기 때문에 GridView의 스크롤 기능 정지
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        mainAxisSpacing: 1,
+        crossAxisSpacing: 1,
+        childAspectRatio: 1,
+      ),
+      itemCount: imageList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _photoWidget(imageList[index], 200, builder: (data) {
+          return GestureDetector(
+            onTap: () {
+              selectedImage = imageList[index];
+              update();
+            },
+            child: Opacity(
+              opacity: imageList[index] == selectedImage ? 0.3 : 1,
+              child: Image.memory(
+                data,
+                fit: BoxFit.cover,
+              ),
+            ),
           );
+        });
+      },
+    );
+  }
+
+  Widget _photoWidget(AssetEntity asset, int size,
+      {required Widget Function(Uint8List) builder}) {
+    return FutureBuilder(
+        future: asset.thumbnailDataWithSize(ThumbnailSize(size, size)),
+        builder: (_, AsyncSnapshot<Uint8List?> snapshot) {
+          if (snapshot.hasData) {
+            return builder(snapshot.data!);
+          } else {
+            return Container();
+          }
         });
   }
 
